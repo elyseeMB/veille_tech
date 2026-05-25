d9bb8369e2e4dd92774cb762915a1543ff1409ea
elysee <mboussaemmanuelito@gmail.com>
2026-05-25 20:13:03 +0100

    style(root/thumbnail): update thumbnailItem

--- apps/root/src/components/ThumbnailItem.tsx
+++ apps/root/src/components/ThumbnailItem.tsx
@@ -22,7 +22,7 @@ export function ThumbnailItem({
           (e.currentTarget as HTMLImageElement).style.display = "none";
         }}
       />
-      <span className="font-medium text-muted-foreground max-w-[180px] truncate">
+      <span className="font-medium text-muted-foreground max-w-[150px] truncate">
         {domain}
       </span>
     </div>
