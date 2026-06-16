903bfb62f3b3b8231ec2ac7ece79c417ff826f5c
elysee <mboussaemmanuelito@gmail.com>
2026-06-16 12:19:51 +0100

    style(front/PageError): update ui

--- apps/root/src/components/PageError.tsx
+++ apps/root/src/components/PageError.tsx
@@ -1,11 +1,11 @@
 import { useLocation, useRouteError } from "react-router";
 import { useEffect, useRef } from "react";
-import { CrossIcon } from "lucide-react";
+import { AlertTriangle } from "lucide-react";
 import Logo from "./Logo.tsx";
 import { Button } from "./ui/button.tsx";
 
 const classNames = {
-  wrapper: "grid place-items-center h-full py-10 text-center space-y-2 ",
+  wrapper: "grid place-items-center h-full text-center space-y-2 ",
   card: "flex flex-col gap-5 p-10 w-1/3 border border-border rounded-xl bg-secondary/50",
   title: "text-2xl flex gap-2 font-semibold items-center justify-center",
   description: "text-base text-txt-tertiary",
@@ -35,13 +35,22 @@ export function PageError({ resetErrorBoundary, error: propsError }: Props) {
   if (!error) {
     return (
       <div className={classNames.wrapper}>
-        <h1 className={classNames.title}>
-          <CrossIcon size={26} />
-          Page not found
-        </h1>
-        <p className={classNames.description}>
-          The page you are looking for does not exist
-        </p>
+        <div className={classNames.card}>
+          <div className="m-auto">
+            <Logo />
+          </div>
+          <span className="text-4xl font-bold text-amber-200">404</span>
+          <h1 className={classNames.title}>
+            <AlertTriangle className="fill-amber-200 stroke-black" size={30} />
+            Page not found
+          </h1>
+          <p className={classNames.description}>
+            The page you are looking for does not exist
+          </p>
+          <Button variant="default" render={<a href="/" />}>
+            Back to home page
+          </Button>
+        </div>
       </div>
     );
   }
